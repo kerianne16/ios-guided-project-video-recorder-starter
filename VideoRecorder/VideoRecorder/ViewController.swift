c39521cf2ded7739c8c92a4f5d605819c252dbb8
Keri Levesque <kerianne166@gmail.com>
2020-04-08 12:33:54 -0400

    added permission, requested access

--- VideoRecorder/VideoRecorder/ViewController.swift
+++ VideoRecorder/VideoRecorder/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
 	
@@ -20,10 +21,38 @@ class ViewController: UIViewController {
 		
 		// TODO: get permission
 		
+        
 		showCamera()
 		
 	}
-	
+   private func requestPermissionAndShowCamera() {
+       let status = AVCaptureDevice.authorizationStatus(for: .video)
+       switch status {
+           case .notDetermined: // 1st run and the user hasn't been asked to give permission
+               requestPermission()
+           case .restricted: // Parental controls limit access to video
+               fatalError("Your don't have permission to use the camera, talk to your parent about enabling")
+           case .denied: // 2nd+ run, the user didn't trust us, or they said no by accident (show how to enable)
+               fatalError("Show them a link to settings to get access to video")
+           case .authorized: // 2nd+ run, they've given permission to use the camera
+               showCamera()
+           @unknown default:
+               fatalError("Didn't handle a new state for AVCaptureDevice authorization")
+       }
+   }
+
+	//pop up only shows up once never again after even if you call it
+
+    private func requestPermission() {
+        AVCaptureDevice.requestAccess(for: .video) { (granted) in
+            guard granted else {
+                fatalError("Tell user they need to give video permission")
+            }
+            DispatchQueue.main.async {
+                self.showCamera()
+            }
+        }
+    }
 	private func showCamera() {
 		performSegue(withIdentifier: "ShowCamera", sender: self)
 	}
